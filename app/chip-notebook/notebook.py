f9bac638a585897f5ab38453bbc04235939fd0d9
patricab <patric.berthelsen@gmail.com>
2022-10-27 09:07:50 +0200

    WIP: parse trace data

--- app/chip-notebook/notebook.py
+++ app/chip-notebook/notebook.py
@@ -2,6 +2,7 @@
 import chipwhisperer.analyzer as cwa
 # print("Hello world")
 
+trace = []
 with open("../aes-example/traces/trace00000", "rb") as f:
 
     # lines = f.readlines()
@@ -9,4 +10,21 @@ with open("../aes-example/traces/trace00000", "rb") as f:
     #     print(line.rstrip())
     
     while f.read(1):
-        print(f.read(1))
+        trace.append(int.from_bytes(f.read(1), "little"))
+        # print(f.read(1))
+
+trace = np.array(trace)
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as sig
+q = 3
+
+# n = np.linspace(0, len(trace), len(trace))
+n = np.linspace(0, len(trace), len(trace))
+n = sig.decimate(n, q)
+trace = sig.decimate(trace, q)
+plt.plot(n, trace)
+plt.show()
+
+# %%
